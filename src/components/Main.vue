6ae5bf9e864fb8d042e8d24ba9bd472381d60a83
David Silva <contatodavidmaia@gmail.com>
2022-11-25 18:51:54 -0300

    :bug: fix: ajustes nas URLs públicas

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -5,19 +5,25 @@
   import Contact from './sections/Contact.vue'
   import Footer from './Footer.vue'
   import Divisor from './Divisor.vue'
+
+  import waves from '@/assets/img/bg/waves.svg'
+  import mobileWaves from '@/assets/img/bg/mobile_waves.svg'
+  import peaks from '@/assets/img/bg/peaks_b.svg'
+  import peaksB from '@/assets/img/bg/peaks.svg'
+  import footer from '@/assets/img/bg/footer.svg'
 </script>
 
 <template>
   <main class="w-full flex flex-col bg-neutral-900 pt-24">
     <About />
-    <Divisor class="hidden lg:block" :src="'src/assets/img/bg/waves.svg'" />
-    <Divisor class="lg:hidden" :src="'src/assets/img/bg/mobile_waves.svg'" />
+    <Divisor class="hidden lg:block" :src="waves" />
+    <Divisor class="lg:hidden" :src="mobileWaves" />
     <Skills />
-    <Divisor :src="'src/assets/img/bg/peaks_b.svg'" />
+    <Divisor :src="peaks" />
     <Projects />
-    <Divisor :src="'src/assets/img/bg/peaks.svg'" />
+    <Divisor :src="peaksB" />
     <Contact />
-    <Divisor :src="'src/assets/img/bg/footer.svg'" />
+    <Divisor :src="footer" />
     <Footer />
   </main>
 </template>
